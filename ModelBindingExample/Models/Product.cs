7aae861b06f0950c131009ff9698a79e427eb376
JoeProgrammer88 <JoeProgrammer88@users.noreply.github.com>
2024-07-11 11:01:30 -0700

    Created a Product model for NewEgg products and added validation

---
+++ ModelBindingExample/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingExample.Models
+{
+    /// <summary>
+    /// Represent a single salable product
+    /// for Newegg.com
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The user facing full name of the product
+        /// </summary>
+        [MaxLength(200)]
+        [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "Only alphanumeric characters are allowed.")]
+        public required string ProductName { get; set; }
+
+        /// <summary>
+        /// Retail price of the item. Does
+        /// not include tax
+        /// </summary>
+        [Range(0, 100_000)]
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Percent the item is on sale, number
+        /// must be between 0 and 100 (0 - 100%)
+        /// </summary>
+        [Range(0, 100)]
+        public int SalePercentage { get; set; }
+
+        /// <summary>
+        /// User facing description of the product
+        /// </summary>
+        public required string ProductDescription { get; set; } = "No description given";
+
+        /// <summary>
+        /// The company that is selling the product
+        /// </summary>
+        public string SoldBy { get; set; }
+    }
+}
